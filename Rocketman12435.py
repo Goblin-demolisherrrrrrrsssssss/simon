f132be98ec9d9e6a4d777fcaf4b0e8c978a40f80
rocketman12435 <125813272+rocketman12435@users.noreply.github.com>
2025-11-17 09:56:24 +0000

    Implement color sequence game
    Added a color sequence game that prompts user input.

--- Rocketman12435.py
+++ Rocketman12435.py
@@ -1,1 +1,13 @@
-
+import random
+colours = ["GREEN","BLUE","RED","YELLOW"]
+sequence = []
+repeat = True
+while repeat:
+    displaycolour = random.choice(colours)
+    sequence.append(displaycolour)
+    print(*sequence,sep = ",")
+    for x in range(len(sequence)):
+        colourinput = input("Colour input:")
+        if colourinput.upper() != sequence:
+            print("Oops! Wrong colour!")
+            repeat = False
