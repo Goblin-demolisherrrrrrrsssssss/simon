ee3ea03d9f7fc065f29b2243e496b234b6e854a1
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2025-11-17 09:45:11 +0000

    Implement user registration and login with hashing
    Added user registration and login functionality with password hashing.

--- simon.py
+++ simon.py
@@ -1,1 +1,37 @@
+import hashlib
+
+def userAdder():
+    fileAdder = open("info.txt", "a")
+    newName = str(input("enter your new username: "))
+    hashedNewName = hashlib.sha256(newName.encode(encoding="utf-8")).hexdigest()
+    newPass = str(input("enter your new password: "))
+    hashedNewPass = hashlib.sha256(newPass.encode(encoding="utf-8")).hexdigest()
+    fileAdder.write(hashedNewName+"\n"+hashedNewPass+"\n")
+    fileAdder.close()
+newUser = input("do you want to register a new user? (y/n): ")
+if newUser == "y":
+    userAdder()
+email = input("please enter your email here: ")
+name = input("please enter your name here: ")
+hash1 = hashlib.sha256((name.encode(encoding="utf-8"))).hexdigest()
+password = input("please enter your password here: ")
+hash2 = hashlib.sha256(password.encode(encoding="utf-8")).hexdigest()
+file = open("info.txt", "r")
+fileContents = file.read()
+file.close()
+splitFileContents = fileContents.splitlines()
+x = int(0)
+userVerified = False
+passVerified = False
+while x<int(len(splitFileContents)):
+    if hash1 == splitFileContents[x]:
+        userVerified = True
+    elif hash2 == splitFileContents[x]:
+        passVerified = True
+    x = x + 1
+if userVerified == True and passVerified == True:
+    print("user has logged in")
+else:
+    print("user has entered something wrong...")
+    sys.exit()
 
