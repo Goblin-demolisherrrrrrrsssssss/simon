d8db0fcaa32993505fab6bb1c0f056650df88f05
rocketman12435 <125813272+rocketman12435@users.noreply.github.com>
2025-11-18 18:40:11 +0000

    Update rocketman.py

--- rocketman.py
+++ rocketman.py
@@ -1,27 +1,30 @@
+import tkinter as tk
+import time
+userSequence=[]
+wrong = False
 import random 
-try: 
-    test = open("points.txt", "r") 
-    test.close() 
-except FileNotFoundError: 
-    pointFileCreator = open("points.txt", "w") 
-    pointFileCreator.close() 
-colours = ["GREEN","BLUE","RED","YELLOW"] 
-sequence = [] 
-repeat = True
+sequence = []
+root = tk.Tk()
+colours = ["GREEN","BLUE","RED","YELLOW"]
 points = 0
 name = ""
-while repeat:
-    displayColour = random.choice(colours)
-    sequence.append(displayColour)
-    print(*sequence,sep = ",")
-    for x in range(len(sequence)):
-        colourInput = input("Colour input:")
-        if colourInput.upper() != sequence:
-            print("Oops! Wrong colour!")
-            break
-        else:
-            points += 1
-print(f"Game Over! You scored {points} points!")
-pointAdder = open("points.txt", "a")
-pointAdder.write(f"{name}: {points}\n")
-pointAdder.close()
+def newRound():
+    userSequence.clear()
+    sequence.append(random.choice(colours))
+    print(sequence)
+
+def buttonClick(colour):
+    global points
+    userSequence.append(colour)
+    print(f"pressed: {colour}")
+    for x in range(len(userSequence)):
+        if userSequence[x] != sequence[x]:
+            print("Oops! Wrong colour. Game over.")
+            print(f"Game Over! You scored {points} points!")
+            pointAdder = open("points.txt", "a")
+            pointAdder.write(f"{name}: {points}\n")
+            pointAdder.close()
+    if userSequence == sequence:
+        print("Correct!")
+        points += 1
+        root.after(600,newRound)
